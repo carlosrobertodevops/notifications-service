bc3bf9b9da1ec8d07daf311ae9a36333b3d8c9d1
“tiassiisspal” <ti.assii.sspal@gmail.com>
2023-02-14 11:44:33 -0300

    Infra - ajustes geral

--- src/infra/app.controller.spec.ts
+++ src/infra/app.controller.spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { AppController } from './app.controller';
+import { AppController } from './http/controllers/app.controller';
 import { PrismaService } from './prisma.service';
 
 describe('AppController', () => {
