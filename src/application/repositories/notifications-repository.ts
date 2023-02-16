08c915cd0cef75dc1edfdd4d994ee134e4da9df5
carlosrobertodevops <carlos.roberto.devops@gmail.com>
2023-02-16 10:15:21 -0300

    findById - criando a class e rota

--- src/application/repositories/notifications-repository.ts
+++ src/application/repositories/notifications-repository.ts
@@ -2,4 +2,6 @@ import { Notification } from '../entities/notification';
 
 export abstract class NotificationsRepository {
   abstract create(notification: Notification): Promise<void>;
+  abastract findByIde(notificationId: string): Promise<Notification | null>;
+
 }
